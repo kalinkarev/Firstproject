660b89a34c793ed5c9a63ef94b56ac9eb66f3e26
Kalin <thegroup1516@gmail.com>
2016-12-16 19:48:11 +0200

    Example from Vogella working

---
+++ SQL_Example/ExampleSQLlite/app/src/main/java/com/karev/android/examplesqllite/Comment.java
@@ -0,0 +1,34 @@
+package com.karev.android.examplesqllite;
+
+/**
+ * Created by kalin on 03.12.16.
+ */
+
+public class Comment {
+
+    private long id;
+    private String comment;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    // Will be used by the ArrayAdapter in the ListView
+    @Override
+    public String toString() {
+        return comment;
+    }
+
+}
